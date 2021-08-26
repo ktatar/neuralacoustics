0e25533696a7a9618db469d5c9dd5be94150d6a7
ktatar <kivanctatar@gmail.com>
2021-08-25 22:04:12 -0700

    adding the code first time

---
+++ neuralacoustics/model.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import operator
+from functools import reduce
+
+import numpy as np
+#VIC this is the content of: https://github.com/zongyi-li/fourier_neural_operator/blob/master/fourier_2d_time.py
+# it needs to be updated!
+# i made a small modification to the original code, please try to preserve it when updating it
+# the mod is highlighted by the following text #VIC-mod
+
+################################################################
+# fourier layer
+################################################################
+
+class SpectralConv2d_fast(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d_fast, self).__init__()
+
+        """
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        #Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+
+class SimpleBlock2d(nn.Module):
+    def __init__(self, modes1, modes2, width, t_in):
+        super(SimpleBlock2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, c=12)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        #self.fc0 = nn.Linear(12, self.width)
+        # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        
+        #VIC-mod t_in is passed as parameter now, so that we can decide the number of input time steps
+        self.fc0 = nn.Linear(t_in+2, self.width)
+        # input channel: the solution of the previous t_in timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+
+
+        self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.w0 = nn.Conv1d(self.width, self.width, 1)
+        self.w1 = nn.Conv1d(self.width, self.width, 1)
+        self.w2 = nn.Conv1d(self.width, self.width, 1)
+        self.w3 = nn.Conv1d(self.width, self.width, 1)
+        self.bn0 = torch.nn.BatchNorm2d(self.width)
+        self.bn1 = torch.nn.BatchNorm2d(self.width)
+        self.bn2 = torch.nn.BatchNorm2d(self.width)
+        self.bn3 = torch.nn.BatchNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        size_x, size_y = x.shape[1], x.shape[2]
+
+        grid = self.get_grid(batchsize, size_x, size_y, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn0(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv1(x)
+        x2 = self.w1(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn1(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv2(x)
+        x2 = self.w2(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn2(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv3(x)
+        x2 = self.w3(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn3(x1 + x2)
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+
+    def get_grid(self, batchsize, size_x, size_y, device):
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+
+class Net2d(nn.Module):
+    def __init__(self, modes, width, t_in):
+        super(Net2d, self).__init__()
+
+        """
+        A wrapper function
+        """
+
+        self.conv1 = SimpleBlock2d(modes, modes, width, t_in)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
+
+    def count_params(self):
+        c = 0
+        for p in self.parameters():
+            c += reduce(operator.mul, list(p.size()))
+
+        return c
---
+++ train.py
@@ -0,0 +1,244 @@
+#VIC put here all the new imports that may be needed
+
+import numpy as np
+import scipy.io
+import h5py
+import sklearn.metrics
+from scipy.ndimage import gaussian_filter
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+# from utilities3 import *
+
+import operator
+from functools import reduce
+from functools import partial
+
+from timeit import default_timer
+
+import os, sys, configparser, argparse
+
+from neuralacoustics.utils import LpLoss
+from neuralacoustics.model import Net2d
+from neuralacoustics.dataset import dataset_loader
+ 
+# KIVANC: We should check determinism on pytorch. There is a full article about it, 
+# and there is a way to force pytorch to be strictly deterministic. 
+# If we go this way, we should avoid any numpy ops, and use torch ops at all times. 
+torch.manual_seed(0)
+np.random.seed(0)
+
+#Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default ='./default.ini' , help='path to the config file')
+args = parser.parse_args()
+
+#Get configs
+config_path = args.config
+config = configparser.ConfigParser(allow_no_value=True)
+
+try: 
+  config.read(config_path)
+
+except FileNotFoundError:
+  print('Config File Not Found at {}'.format(config_path))
+  sys.exit()
+
+# simulation parameters
+ntrain = config['simulation'].getint('ntrain')
+ntest = config['simulation'].getint('ntest')
+
+modes = config['network'].getint('modes')
+width = config['network'].getint('width')
+
+batch_size = config['training'].getint('batch_size')
+
+epochs = config['training'].getint('epochs')
+learning_rate = config['training'].getfloat('learning_rate')
+scheduler_step = config['training'].getint('scheduler_step')
+scheduler_gamma = config['training'].getfloat('scheduler_gamma')
+
+print(epochs, learning_rate, scheduler_step, scheduler_gamma)
+
+
+T_in = config['simulation'].getint('window_size')
+T_out = T_in
+# T_in+T_out is window size!
+
+win_stride = config['simulation'].getint('win_stride')
+win_lim = config['simulation'].getint('win_lim')
+
+#(T_in+T_out)*200 #-1 for no limit
+
+# dataset
+dataset_name = config['dataset'].get('name')
+
+#-------------------------------------------------------------------------------
+
+
+
+MODEL_ID = config['dataset'].get('model_id')
+
+dataset_path = config['dataset'].get('path')
+
+# retrieve dataset details and check them
+splitname = dataset_name.split('_')
+
+DATASET = splitname[1]
+
+S = splitname[4]
+S = int(S[1:])
+
+mu = splitname[5][2:]
+rho = splitname[6][3:]
+gamma = splitname[7][5:]
+
+# prepare to save model
+model_name = 'iwe_m'+MODEL_ID+'_'+DATASET+'_n'+str(ntrain)+'+'+str(ntest)+'_e'+str(epochs)+'_m'+str(modes)+'_w'+ str(width)+'_ti'+str(T_in)+'_to'+str(T_out)+'_ws'+str(win_stride)+'_wl'+str(win_lim)+'_s'+str(S)+'_m'+mu+'_r'+rho+'_g'+gamma
+
+model_path = dataset_path[:-9]+'/models/'
+
+#VIC i think this needs only a minor update, i.e., removing the padding of the location
+
+t1 = default_timer()
+
+u = dataset_loader(dataset_name, dataset_path, ntrain+ntest, T_in+T_out, win_stride, win_lim)
+
+train_a = u[:ntrain,:,:,:T_in]
+train_u = u[:ntrain,:,:,T_in:T_in+T_out]
+
+ntest_start = ntrain
+test_a = u[ntest_start:ntest_start+ntest,:,:,:T_in]
+test_u = u[ntest_start:ntest_start+ntest:,:,:,T_in:T_in+T_out]
+
+#test_a = u[-ntest:,:,:,:T_in]
+#test_u = u[-ntest:,:,:,T_in:T_in+T_out]
+
+
+print(train_u.shape, test_u.shape)
+assert (S == train_u.shape[-2])
+assert (T_out == train_u.shape[-1])
+
+train_a = train_a.reshape(ntrain,S,S,T_in)
+test_a = test_a.reshape(ntest,S,S,T_in)
+
+#VIC should be removed
+# pad the location (x,y)
+# gridx = torch.tensor(np.linspace(0, 1, S), dtype=torch.float)
+# gridx = gridx.reshape(1, S, 1, 1).repeat([1, 1, S, 1])
+# gridy = torch.tensor(np.linspace(0, 1, S), dtype=torch.float)
+# gridy = gridy.reshape(1, 1, S, 1).repeat([1, S, 1, 1])
+
+# train_a = torch.cat((gridx.repeat([ntrain,1,1,1]), gridy.repeat([ntrain,1,1,1]), train_a), dim=-1)
+# test_a = torch.cat((gridx.repeat([ntest,1,1,1]), gridy.repeat([ntest,1,1,1]), test_a), dim=-1)
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_a, train_u), batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(test_a, test_u), batch_size=batch_size, shuffle=False)
+
+t2 = default_timer()
+
+print('preprocessing finished, time used:', t2-t1, 's')
+print('train input shape:',train_a.shape, ' output shape: ', train_u.shape)    
+
+#VIC this needs a couple of touch ups, as at the bottom of: https://github.com/zongyi-li/fourier_neural_operator/blob/master/fourier_2d_time.py
+# also, i made very minor changes to the original code, to make the logic clearer
+
+if torch.cuda.is_available() :
+  model = Net2d(modes, width, T_in).cuda()
+  device  = torch.device('cuda')
+  print(" Utilizing CUDA")  
+else :
+  model = Net2d(modes, width, T_in)
+  device  = torch.device('cpu')
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(torch.cuda.current_device()))
+print(torch.cuda.is_available())    
+
+
+print(model.count_params())
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+
+# myloss = LpLoss(size_average=False)
+
+#VIC these are not needed anymore
+# gridx = gridx.to(device)
+# gridy = gridy.to(device)
+
+
+
+myloss = LpLoss(size_average=False)
+step = 1
+for ep in range(epochs):
+    model.train()
+    t1 = default_timer()
+    train_l2_step = 0
+    train_l2_full = 0
+    for xx, yy in train_loader:
+        loss = 0
+        xx = xx.to(device)
+        yy = yy.to(device)
+
+        for t in range(0, T_out, step):
+            y = yy[..., t:t + step]
+            im = model(xx)
+            loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+
+            if t == 0:
+                pred = im
+            else:
+                pred = torch.cat((pred, im), -1)
+
+            xx = torch.cat((xx[..., step:], im), dim=-1)
+
+        train_l2_step += loss.item()
+        l2_full = myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1))
+        train_l2_full += l2_full.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    test_l2_step = 0
+    test_l2_full = 0
+    with torch.no_grad():
+        for xx, yy in test_loader:
+            loss = 0
+            xx = xx.to(device)
+            yy = yy.to(device)
+
+            for t in range(0, T_out, step):
+                y = yy[..., t:t + step]
+                im = model(xx)
+                loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+
+                if t == 0:
+                    pred = im
+                else:
+                    pred = torch.cat((pred, im), -1)
+
+                xx = torch.cat((xx[..., step:], im), dim=-1)
+
+            test_l2_step += loss.item()
+            test_l2_full += myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1)).item()
+
+    t2 = default_timer()
+    scheduler.step()
+    print(ep, t2 - t1, train_l2_step / ntrain / (T_out / step), train_l2_full / ntrain, test_l2_step / ntest / (T_out / step),
+          test_l2_full / ntest)
+    
+# add loss to name, with 4 decimals    
+final_training_loss = '{:.4f}'.format(test_l2_full / ntest)
+final_training_loss = final_training_loss.replace('.', '@')
+
+model_name = model_name+'_loss'+final_training_loss   
+model_full_path = model_path+model_name
+
+torch.save(model, model_full_path)
+
+#path_train_err = model_path+'results/'+model_name+'_train.txt'
+#path_test_err = model_path+'results/'+model_name+'_test.txt'
