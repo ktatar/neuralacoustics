990e86870467723873df28275c0e330bea0c677e
ghgpin <108945383+ghgpin@users.noreply.github.com>
2022-08-15 16:18:37 -0400

    added chunks, saving functionality, removed generator_model variables, removed determinism

--- generate_dataset.py
+++ generate_dataset.py
@@ -23,16 +23,11 @@ config = getConfigParser(prj_root, __file__) # we call this script from command 
 
 generator_name = config['dataset_generation'].get('generator_name') #name of generator file 
 
-generator_root_ = config['dataset_generation'].get('generator_dir') # generator_root_ variable only used for generator_config_path now.
-generator_root = generator_root_.replace('PRJ_ROOT', prj_root) 
-generator_root = Path(generator_root) #path for generator root(just the numerical model root for now)
+generator_dir_ = config['dataset_generation'].get('generator_dir')
+generator_dir = generator_dir_.replace('PRJ_ROOT', prj_root) 
+generator_dir = Path(generator_dir) #path for generator root
 
-generator_model = config['dataset_generation'].get('generator_model')# this is the numerical model used for dataset generation.
-
-generator_dir = generator_root.joinpath(generator_model) # model_dir = model_root/model_name_ -> it is folder, where model script and its config file reside
-#right now, generator file and model file are stored in same directory.
-
-# generator config file (basically just the model config file code with names changed.)
+# generator config file
 generator_config_path = config['dataset_generation'].get('generator_config')
 
 # default config has same name as generator and is in same folder
@@ -42,31 +37,18 @@ else:
   generator_config_path = generator_config_path.replace('PRJ_ROOT', prj_root)
   generator_config_path = Path(generator_config_path)
 
-#-------------------------------------------------------------------------------
-# load model (generator)
-#will need to tweak if generator file is moved to a diff folder(currently in numerical_models/num_model_3)
+# chunks
+ch = config['dataset_generation'].getint('chunks') # num of chunks
 
-# we want to load the package through potential subfolders
-# we can pretend we are in the PRJ_ROOT, for __import__ will look for the package from there
-generator_path_folders = Path(generator_root_.replace('PRJ_ROOT', '.')).joinpath(generator_model).parts # also add folder with same name as model
-#should the above be replaced with just generator_root.joinpath(generator_model).parts? both seem to work
+# dataset dir
+dataset_dir_ = config['dataset_generation'].get('dataset_dir') #where to save files.
+dataset_dir = dataset_dir_.replace('PRJ_ROOT', prj_root)
 
-# create package structure by concatenating folders with '.'
-packages_struct = generator_path_folders[0]
-for pkg in range(1,len(generator_path_folders)):
-    packages_struct += '.'+generator_path_folders[pkg] 
-# load
+#device
+dev_ = config['dataset_generation'].get('dev') # cpu or gpu, keep original for dataset config
+dev = dev_
 
-generator = __import__(packages_struct + '.' + generator_name, fromlist=['*']) # model.path.model_name_ is model script [i.e., package].
-
-
-#-------------------------------------------------------------------------------
-torch.use_deterministic_algorithms(True) #enables determinism.
-torch.backends.cudnn.deterministic = True 
-#torch.manual_seed(0) #sets seed
-
-#-------------------------------------------------------------------------------
-dev = config['dataset_generation'].get('dev') # cpu or gpu, keep original for dataset config
+#-------------------------------------------------------------------------------------
 
 # in case of generic gpu or cuda explicitly, check if available
 if dev == 'gpu' or 'cuda' in dev:
@@ -81,6 +63,126 @@ if dev == 'gpu' or 'cuda' in dev:
 print('Device:', dev)
 
 #------------------------------------------------------------------------------
+# load generator
+#will need to tweak if generator file is moved to a diff folder(currently in numerical_models/num_model_3)
 
-generator.load_generator(generator_config_path, prj_root) #passing prj_root to maintain original code structure.
-generator.generate_dataset(dev) #generates dataset.
+# we want to load the package through potential subfolders
+# we can pretend we are in the PRJ_ROOT, for __import__ will look for the package from there
+generator_path_folders = generator_dir.parts
+
+# create package structure by concatenating folders with '.'
+packages_struct = generator_path_folders[0]
+for pkg in range(1,len(generator_path_folders)):
+    packages_struct += '.'+generator_path_folders[pkg] 
+# load
+
+generator = __import__(packages_struct + '.' + generator_name, fromlist=['*']) # model.path.model_name_ is model script [i.e., package].
+
+#-------------------------------------------------------------------------------
+num_of_batches, ch, rem = generator.load(generator_config_path, ch) #return number of batches, chunks, remainder, after loading
+
+batches_per_ch = num_of_batches//ch
+ch_size = batches_per_ch * generator.B # num of data points per chunk
+
+if num_of_batches == -1:
+    generator.generate_datasetBatch(dev) #generate 1 batch with display on.
+    
+else:   
+    # compute name of dataset + create folder
+    #----------------------------------------------------------------------------
+    # count datasets in folder
+    datasets = list(Path(dataset_dir).glob('*'))
+    num_of_datasets = len(datasets)
+    # choose new dataset index accordingly
+    DATASET_INDEX = str(num_of_datasets)
+
+    name_clash = True
+
+    while name_clash:
+        name_clash = False
+        for dataset in datasets:
+            # in case a dataset with same name is there
+            if Path(dataset).parts[-1] == 'dataset_'+DATASET_INDEX:
+                name_clash = True
+                DATASET_INDEX = str(int(DATASET_INDEX)+1) # increase index
+
+    dataset_name = 'dataset_' + DATASET_INDEX
+    dataset_folder = Path(dataset_dir).joinpath(dataset_name)
+
+    # create folder where to save dataset
+    dataset_folder.mkdir(parents=True, exist_ok=True)
+    
+        
+    #----------------------------------------------------------------------------
+    # compute number of leading zeros for pretty file names
+    ch_num = str(ch)
+    l_zeros=len(ch_num) # number of leading zeros in file name
+    # check if l_zeros needs to be lowered down
+    # e.g., ch_num = 100 -> [0, 99] -> should be printed with only one leading zero:
+    # 01, 02, ..., 98, 99
+    cc = pow(10,l_zeros-1)
+    if ch <= cc:
+        l_zeros = l_zeros-1
+    
+    #-------------------------------------------------------------------------------
+        
+    time_duration = generator.nsteps * generator.dt
+    print('simulation duration: ', time_duration, 's')
+    
+    # initial conditions
+    #a = torch.zeros(ch_size, h, w) #VIC we will re-introduce at a certain point, to save continous excitation and other parameters, like mu and boundaries [first static then dynamic]
+    
+    # solutions
+    u = torch.zeros(ch_size, generator.h, generator.h, generator.nsteps+1) # +1 becase initial condition is saved at beginning of solution time series!
+    
+    
+    ch_cnt = 0 #keeps track of # of chunks, datapoints during loop.
+    n_cnt=0
+    
+    t1 = default_timer()
+    for b in range(num_of_batches):
+
+        # compute all steps in full batch
+        sol, sol_t = generator.generate_datasetBatch(dev) #generates dataset.
+        
+        # store
+        u[n_cnt:(n_cnt+generator.B),...] = sol # results
+
+        n_cnt += generator.B
+
+        # save some chunk, just in case...
+        if (b+1) % batches_per_ch == 0: 
+          file_name = dataset_name + '_ch' + str(ch_cnt).zfill(l_zeros) + '_' + str(n_cnt) + '.mat'
+          dataset_path = dataset_folder.joinpath(file_name)
+          #scipy.io.savemat(dataset_path, mdict={'a': a.cpu().numpy(), 'u': u.cpu().numpy(), 't': sol_t.cpu().numpy()})
+          scipy.io.savemat(dataset_path, mdict={'u': u.cpu().numpy(), 't': sol_t.cpu().numpy()})
+          print( '\tchunk {}, {} dataset points  (up to batch {} of {})'.format(ch_cnt, ch_size, b+1, num_of_batches) )
+          ch_cnt += 1
+          # reset initial conditions, solutions and data point count
+          u = torch.zeros(ch_size, generator.h, generator.h, generator.nsteps+1) # +1 becase initial condition is repeated at beginning of solution time series!
+          n_cnt = 0
+          
+        elif (b+1) == num_of_batches:
+          file_name = dataset_name + '_rem_' + str(n_cnt)  + '.mat'
+          dataset_path = dataset_folder.joinpath(file_name)
+          scipy.io.savemat(dataset_path, mdict={'u': u.cpu().numpy(), 't': sol_t.cpu().numpy()})
+          print( '\tremainder, {} dataset points (up to batch {} of {})'.format(n_cnt, b+1, num_of_batches) )
+          rem = 1
+
+    t2 = default_timer()
+
+    rem_size = rem * n_cnt
+    actual_size = (ch_size * ch) + rem_size
+
+    print(f'\nDataset {dataset_name} saved in:')
+    print('\t', dataset_folder)
+    print(f'total number of data points: {actual_size} (out of {generator.N} requested)')
+    if ch == 1:
+        print('in a single chunk')
+    else:
+        print(f'split in {ch_cnt} chunks with {ch_size} datapoints each')
+        if rem:
+          print(f'plus remainder file with {n_cnt} datapoints')
+
+    simulation_duration = t2-t1
+    print(f'\nElapsed time: {simulation_duration} s\n')
