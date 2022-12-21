eece162684170e5a4e89488bcea69c92d672cdf7
ghgpin <108945383+ghgpin@users.noreply.github.com>
2022-12-21 14:08:53 -0500

    solver now returns excitation

--- numerical_models/linearMembrane_horizontalSinusoid/linearMembrane_horizontalSinusoid.py
+++ numerical_models/linearMembrane_horizontalSinusoid/linearMembrane_horizontalSinusoid.py
@@ -2,7 +2,7 @@ import torch
 import math # for pi and floor
 from pathlib import Path # to properly handle paths and folders on every os
 from neuralacoustics.utils import openConfig
-from neuralacoustics.utils import import_file
+from neuralacoustics.utils import import_fromScript
 
 # to store values from load()
 solver = 0 # where to load solver
@@ -39,6 +39,7 @@ def load(config_path, prj_root):
     _load(solver, prj_root, config_path)  # loads solver
 
     return
+
 
 def load_test(config_path, prj_root):    
     # to prevent python from declaring new local variables with the same names
@@ -105,9 +106,10 @@ def load_test(config_path, prj_root):
 
 def _load(solver_path, prj_root, config_path):
     global solver
-    solver, temp_var = import_file(prj_root, config_path, solver_path)
+    solver, _ = import_fromScript(prj_root, config_path, solver_path)
 
     return
+
 
 def run(dev, b, dt, nsteps, w, h, mu, rho, gamma, ex_input_freq, ex_input_mag, ex_input_phase, disp=False, dispRate=1, pause=0):
     #function will be called by generator, all params passed at runtime (does not use global variables)
@@ -177,9 +179,10 @@ def run(dev, b, dt, nsteps, w, h, mu, rho, gamma, ex_input_freq, ex_input_mag, e
     
     #--------------------------------------------------------------
     # run solver
-    input, sol, sol_t = solver.run(dev, dt, nsteps, b, w, h, _mu, _rho, _gamma, excite, torch.empty(0, 1), disp, dispRate, pause)
+    full_excitation, sol = solver.run(dev, dt, nsteps, b, w, h, _mu, _rho, _gamma, excite, torch.empty(0, 1), disp, dispRate, pause)
 
-    return [input, sol, sol_t]
+    return [full_excitation, sol]
+
 
 def run_test(dev, dispRate=1, pause=0):
     # set parameters
@@ -188,9 +191,9 @@ def run_test(dev, dispRate=1, pause=0):
     
     
     #call run using those parameters+global variables, and return the result.
-    test_input, test_sol, test_sol_t = run(dev, _b, dt, nsteps, w, h, mu, rho, gamma, ex_input_freq, ex_input_mag, ex_input_phase, _disp, dispRate, pause)
+    test_excite, test_sol = run(dev, _b, dt, nsteps, w, h, mu, rho, gamma, ex_input_freq, ex_input_mag, ex_input_phase, _disp, dispRate, pause)
     
-    return [test_input, test_sol, test_sol_t]
+    return [test_excite, test_sol]
 
 
 def getSolverInfo():
