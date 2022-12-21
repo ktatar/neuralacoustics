c82b322cd7d52e8d92920438685009b2da784408
ghgpin <108945383+ghgpin@users.noreply.github.com>
2022-12-21 14:20:03 -0500

    solver now returns excitation, instead of state

--- numerical_models/linearMembrane_localNoise/linearMembrane_localNoise.py
+++ numerical_models/linearMembrane_localNoise/linearMembrane_localNoise.py
@@ -1,7 +1,7 @@
 import torch
 from pathlib import Path # to properly handle paths and folders on every os
 from neuralacoustics.utils import openConfig
-from neuralacoustics.utils import import_file
+from neuralacoustics.utils import import_fromScript
 
 # to store values from load()
 solver = 0 #  where to load solver
@@ -111,7 +111,7 @@ def load_test(config_path, prj_root):
 
 def _load(solver_path, prj_root, config_path):
     global solver
-    solver, temp_var = import_file(prj_root, config_path, solver_path)
+    solver, _ = import_fromScript(prj_root, config_path, solver_path)
 
     return
 
@@ -143,9 +143,9 @@ def run(dev, b, dt, nsteps, w, h, mu, rho, gamma, ex_x, ex_y, noise_submatrix, d
 
     #--------------------------------------------------------------
     # run solver
-    inputs, sol, sol_t = solver.run(dev, dt, nsteps, b, w, h, _mu, _rho, _gamma, excite, torch.empty(0, 1), disp, dispRate, pause)
+    full_excitation, sol = solver.run(dev, dt, nsteps, b, w, h, _mu, _rho, _gamma, excite, torch.empty(0, 1), disp, dispRate, pause)
 
-    return [inputs, sol, sol_t]
+    return [full_excitation, sol]
 
 
 def run_test(dev, dispRate=1, pause=0):
@@ -157,9 +157,9 @@ def run_test(dev, dispRate=1, pause=0):
     noise_submatrix[0, ex_y[0]: ex_y[0]+ex_size[0], ex_x[0]: ex_x[0]+ex_size[0]] = torch.randn(ex_size[0],ex_size[0])
     
     #call run using those parameters+global variables, and return the result.
-    test_inputs, test_sol, test_sol_t = run(dev, _b, dt, nsteps, w, h, mu, rho, gamma, ex_x, ex_y, noise_submatrix, _disp, dispRate, pause)
+    test_excite, test_sol = run(dev, _b, dt, nsteps, w, h, mu, rho, gamma, ex_x, ex_y, noise_submatrix, _disp, dispRate, pause)
     
-    return [test_inputs, test_sol, test_sol_t]
+    return [test_excite, test_sol]
 
 
 def getSolverInfo():
