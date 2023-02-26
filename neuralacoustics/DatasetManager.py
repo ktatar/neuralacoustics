2f6755d422106bead79524a32aedb4dc6b6f7fbd
ghgpin <108945383+ghgpin@users.noreply.github.com>
2023-02-26 18:18:35 -0500

    self.N now uses actual size instead of N, implemented T_in and T_out into checkArgs() and loadData()

--- neuralacoustics/DatasetManager.py
+++ neuralacoustics/DatasetManager.py
@@ -22,7 +22,7 @@ class DatasetManager:
         config = openConfig(self.config_path, Path(__file__).name)
 
         # Read from config file
-        self.N = config['dataset_generator_parameters'].getint('N')
+        self.N = config['dataset_details'].getint('actual_size')
         self.T = config['numerical_model_parameters'].getint('nsteps')
         self.T += 1
         self.w = config['numerical_model_parameters'].getint('w')
@@ -40,28 +40,38 @@ class DatasetManager:
         self.files.remove(self.config_path)  # ignore config file
         self.files = sorted(self.files)
 
-    def checkArgs(self, start_ch, win, stride, win_lim):
+    def checkArgs(self, start_ch, T_in, T_out win, stride, win_lim):
         """
         Check the validity of data query arguments and 
         modify them if necessary.
         """
+        # if either T_in or T_out are 0, or if the window is larger than # of timesteps
+        # set them both to default values (half of full timesteps each)
+        win = T_in + T_out
+        if (T_in == 0 or T_out==0 or win > self.T):
+            T_in = self.T//2
+            T_out = self.T-T_in
+            win = T_in + T_out
+
         # Start from 0 if the requested start chunk file is out of bound
         if (start_ch < 0 or start_ch > self.ch + self.rem - 1):
             start_ch = 0
-        # Each window covers all available timesteps by default
-        if (win <= 0):
-            win = self.T
-        # Windows are juxtaposed by default
+        # Windows  are juxtaposed by default
         if (stride <= 0):
             stride = win
-        # Modify total timestep if window limit is applied
-        if win_lim > 0 and win_lim < self.T and win_lim >= win:
-            self.T = win_lim
+        
+        # if window limit out of range, clip it to all timesteps.
+        if win_lim <= 0 or win_lim > self.T:
+            win_lim = self.T
+
+        # if win_limit is too low, increase it to window size
+        if win_lim < win:
+            win_lim = win
 
         # Check that window size is smaller than number of timesteps per each data entry
         assert (self.T >= win)
 
-        return start_ch, win, stride
+        return start_ch, win, stride, T_in, T_out, win_lim
 
     def checkDatapointNum(self, n, p_num, start_ch):
         """Check whether there are enought datapoints."""
@@ -75,10 +85,10 @@ class DatasetManager:
     def loadData(self, n, win, stride=0, win_lim=0, start_ch=0, permute=False, field='sol'):
         """Load a subsection of dataset for training."""
         # Check and modify arguments
-        start_ch, win, stride = self.checkArgs(start_ch, win, stride, win_lim)
+        start_ch, win, stride, T_in, T_out, win_lim = self.checkArgs(start_ch, T_in, T_out, stride, win_lim)
 
         # number of points per each dataset entry
-        p_num = int((self.T - win) / stride) + 1
+        p_num = int((win_lim - win) / stride) + 1
         # Check whether there's enough datapoints
         self.checkDatapointNum(n, p_num, start_ch)
 
@@ -98,25 +108,30 @@ class DatasetManager:
         cnt = 0
         for f in range(full_files):
             dataloader = MatReader(self.files[f + start_ch])
-            uu = dataloader.read_field(field)
+            excite = dataloader.read_field('excite')
+            sol = dataloader.read_field('sol')
 
             # Unroll all entries with moving window
             for e in range(self.ch_size):
                 for tt in range(p_num):
                     t = tt * stride
-                    u[cnt, ...] = uu[e, :, :, t:t+win]
+                    u[cnt, :, :, 0:T_in] = sol[e, :, :, t:t+T_in] + excite[e, :, :, t+1:t+T_in+1]
+                    u[cnt, :, :, T_in:T_in+T_out] = sol[e, :, :, t+T_in:t+T_in+T_out]
                     cnt += 1
 
         # Load the remaining file
         if (extra_datapoints > 0):
             print(f'\tPlus {extra_datapoints} points from one other file')
             dataloader = MatReader(self.files[start_ch + full_files])
-            uu = dataloader.read_field(field)
+            excite = dataloader.read_field('excite')
+            sol = dataloader.read_field('sol')
             data_entry = 0
             while (cnt < n):
                 for tt in range(p_num):
                     t = tt * stride
-                    u[cnt, ...] = uu[data_entry, :, :, t:t+win]
+                    
+                    u[cnt, :, :, 0:T_in] = sol[data_entry, :, :, t:t+T_in] + excite[data_entry, :, :, t+1:t+T_in+1]
+                    u[cnt, :, :, T_in:T_in+T_out] = sol[data_entry, :, :, t+T_in:t+T_in+T_out]
                     cnt += 1
                     if (cnt >= n):
                         break
@@ -130,8 +145,9 @@ class DatasetManager:
 
         return u
 
-    def loadDataEntry(self, n, win, entry, field='sol'):
+    def loadDataEntry(self, n, T_in, T_out, entry):
         """Load data from one single data entry."""
+        win = T_in + T_out
         # Check validity of entry index
         if entry >= self.ch * self.ch_size + self.rem_size or entry < 0:
             raise AssertionError("Invalid entry index")
@@ -153,9 +169,12 @@ class DatasetManager:
 
         # Load the target entry
         dataloader = MatReader(self.files[file_index])
-        uu = dataloader.read_field(field)
+        excite = dataloader.read_field('excite')
+        sol = dataloader.read_field('sol')
         for tt in range(n):
-            u[cnt, ...] = uu[entry_in_file, :, :, tt:tt+win]
+            u[cnt, :, :, 0:T_in] = sol[entry_in_file, :, :, tt:tt+T_in] + excite[entry_in_file, :, :, tt+1:tt+T_in+1]
+            u[cnt, :, :, T_in:T_in+T_out] = sol[entry_in_file, :, :, tt+T_in:tt+T_in+T_out]
+
             cnt += 1
 
         return u
